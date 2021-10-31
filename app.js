d08bc3a806df5edbdb8a5f0a914cdeb973219da7
KathileeLedgister <k.alex.ledgister@gmail.com>
2021-10-31 18:09:35 -0500

    Completed Exercise 3

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 
 function onclickSubmit(event) {
 
-    let queryRequest = new Request('superheroes.php');
 
-    fetch(queryRequest)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-                return response.text();
-            })
-            .then(responseText => {
-                alert(responseText);
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-                alert('Error:' + error.message);
-            });
+    do {
+        let queryObj = document.getElementById("textInput");
+        if (!queryObj.validity.valid) {
+            alert("Please enter a valid Avenger Name or Alias");
+            break;
+        }
+        let inputString = queryObj.value;
+        inputString = encodeURIComponent(inputString.trim());
+        let queryRequest = new Request('superheroes.php?query=' + inputString);
+        let svresult = document.getElementById("svresult");
+
+        fetch(queryRequest)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! status: ${response.status}`);
+                    }
+                    return response.text();
+                })
+                .then(responseText => {
+                    svresult.innerHTML = responseText;
+                })
+                .catch((error) => {
+                    svresult.innerHTML = "<h4 class=\"queryerror\">"+'Error:' + error.message+"</h4>";
+                });
+    } while (false);
     event.stopPropagation();
     event.preventDefault();
 }
